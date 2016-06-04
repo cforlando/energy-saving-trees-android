0278ff0c7d8db7b6bc00765f96eb898a8dc35c54
Joshua Donlan <jdonlan@fullsail.com>
2016-06-04 14:46:23 -0400

    Added detail activity files.
    Signed-off-by: Joshua Donlan <jdonlan@fullsail.com>

--- app/src/main/java/com/codefororlando/streettrees/DetailActivity.java
+++ app/src/main/java/com/codefororlando/streettrees/DetailActivity.java
@@ -3,11 +3,22 @@ package com.codefororlando.streettrees;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.codefororlando.streettrees.fragments.DetailFragment;
+
 public class DetailActivity extends AppCompatActivity {
+
+    final String TAG = "DETAILACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        if(savedInstanceState == null) {
+            DetailFragment frag = DetailFragment.newInstance();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.detailContainer, frag, DetailFragment.TAG)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/codefororlando/streettrees/fragments/DetailFragment.java
+++ app/src/main/java/com/codefororlando/streettrees/fragments/DetailFragment.java
@@ -1,7 +1,38 @@
 package com.codefororlando.streettrees.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codefororlando.streettrees.R;
+
 /**
  * Created by jdonlan on 6/4/16.
  */
-public class DetailFragment {
+public class DetailFragment extends Fragment {
+
+    public static final String TAG = "DETAILFRAGMENT";
+
+    public static DetailFragment newInstance() {
+        DetailFragment frag = new DetailFragment();
+
+        Bundle args = new Bundle();
+        frag.setArguments(args);
+
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail_activity, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
 }
