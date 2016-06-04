638eee7a435c68b343fc0b370a22177e3135e5b3
Chris Jones <chris@cjqed.com>
2016-06-04 12:39:10 -0400

    Add API models

---
+++ app/src/main/java/com/codefororlando/streettrees/api/ApiManager.java
@@ -0,0 +1,14 @@
+package com.codefororlando.streettrees.api;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Chris.Jones on 6/4/2016.
+ */
+public class ApiManager {
+//    public AsyncTask<String, String, JSONObject> GetTreesWithinLocation() {
+//
+//    }
+}
---
+++ app/src/main/java/com/codefororlando/streettrees/api/providers/SavedTreesProvider.java
@@ -0,0 +1,8 @@
+package com.codefororlando.streettrees.api.providers;
+
+/**
+ * Created by Chris.Jones on 6/4/2016.
+ */
+public class SavedTreesProvider {
+
+}
