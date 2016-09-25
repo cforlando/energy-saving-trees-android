9b5bfca6d575c33430132bcf73446e556c78bfaf
John Li <jli@unikry.com>
2016-09-25 11:23:55 -0400

    Implemented geojson fencing

--- app/src/main/java/com/codefororlando/streettrees/MainActivity.java
+++ app/src/main/java/com/codefororlando/streettrees/MainActivity.java
@@ -1,20 +1,18 @@
 package com.codefororlando.streettrees;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.codefororlando.streettrees.api.models.Tree;
-import com.codefororlando.streettrees.api.models.TreeDescription;
 import com.codefororlando.streettrees.api.providers.SavedTreesProvider;
-import com.codefororlando.streettrees.api.providers.TreeProvider;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,13 +22,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.VisibleRegion;
+import com.google.maps.android.geojson.GeoJsonLayer;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnCameraChangeListener {
@@ -42,6 +39,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public static final int REQUEST_ACCESS_FINE_LOCATION = 10001;
     public static final String EXTRA_LOCATION = "location";
     public static final String EXTRA_TREETYPE = "type";
+
+    FloatingActionButton fab;
+    String locationProvider;
+
+    public static final int FINE_LOCATION_REQUEST_CODE = 103;   //random number
+    private int DEFAULT_ZOOM_LEVEL = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +64,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.setOnInfoWindowClickListener(this);
         map.setOnCameraChangeListener(this);
 
-        String locationProvider = locationManager.getBestProvider(new Criteria(), true);
+        locationProvider = locationManager.getBestProvider(new Criteria(), true);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
             //TODO: REQUEST PERMISSION - CONCERN RELEASE VERSION TARGET 16
             //requestPermissions(new String[]{"android.permission.ACCESS_FINE_LOCATION"},REQUEST_ACCESS_FINE_LOCATION);
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
@@ -71,13 +75,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Log.d(TAG,"Setting Map Location");
-        Location myLocation = locationManager.getLastKnownLocation(locationProvider);
-        double latitude = myLocation.getLatitude();
-        double longitude = myLocation.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
-        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        map.animateCamera(CameraUpdateFactory.zoomTo(16));
+        centerMapToUser();
+        loadAndSetGeoData();
     }
 
     private void addMarkersToMap(Tree[] trees) {
@@ -97,11 +96,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Intent detailIntent = new Intent(this, DetailActivity.class);
         String treeTame = marker.getTitle();
         LatLng location = marker.getPosition();
-        detailIntent.putExtra(EXTRA_LOCATION,location);
-        detailIntent.putExtra(EXTRA_TREETYPE,treeTame);
+        detailIntent.putExtra(EXTRA_LOCATION, location);
+        detailIntent.putExtra(EXTRA_TREETYPE, treeTame);
         startActivity(detailIntent);
     }
-
 
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
@@ -113,4 +111,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case FINE_LOCATION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    centerMapToUser();
+                } else {
+                    // permission denied, boo! Disable the
+                }
+                return;
+            }
+        }
+    }
+
+    private void loadAndSetGeoData() {
+        try {
+            GeoJsonLayer layer = new GeoJsonLayer(map, R.raw.orlando_city_limits, getApplicationContext());
+            int green = getResources().getColor(R.color.orlandoGreen);
+            int transparentGreen = getResources().getColor(R.color.orlandoGreenTransparent);
+            layer.getDefaultPolygonStyle().setStrokeColor(green);
+            layer.getDefaultPolygonStyle().setFillColor(transparentGreen);
+            layer.addLayerToMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void centerMapToUser() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+        LatLng currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, DEFAULT_ZOOM_LEVEL));
+    }
+
 }
---
+++ app/src/main/java/com/codefororlando/streettrees/api/OrlandoGeoService.java
@@ -0,0 +1,10 @@
+package com.codefororlando.streettrees.api;
+
+/**
+ * Created by johnli on 9/25/16.
+ */
+public class OrlandoGeoService {
+
+    String GeoData = "https://data.cityoforlando.net/api/geospatial/k3h7-bsnc?method=export&format=GeoJSON";
+
+}
